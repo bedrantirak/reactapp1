b38244f1b513e4ffe2478a189a82b42976508611
bedrantirak <bdrntrk@gmail.com>
2020-10-10 10:28:42 +0300

    Jsx formatı

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const test =34;
+  const isAuto=true;
   return (
     <div className="App">
-      <h1>First React Project </h1>
+      <h4>{1+1} </h4>
+      <h4>{"bedran".toUpperCase()}</h4>
+      <h4>{test}</h4>
+      <div>
+        {
+          isAuto ? <p>Kullanıcı kayıtlı</p>
+          : null
+        }
+
+
+      </div>
     </div>
   );
 }
