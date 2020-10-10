b54e2e4ec75c66070896f5d9102b9e333b598798
bedrantirak <bdrntrk@gmail.com>
2020-10-10 13:52:57 +0300

    UI and Font Awesome

--- src/components/User.js
+++ src/components/User.js
@@ -8,22 +8,34 @@ export default class User extends Component {
             department : "Bilgi yok",
              salary : "Bigi yok"
          }
-         
+
     render() {
         //Destructing
         const {name,department,salary} = this.props;
         return (
-            <div>
+            <div className="col-md-8 mb-4">
                 {/* <form>
                     <input type="text" />
                     <button>Send</button>
                 </form> */}
-                <ul>
-                    <li>İsim : {name} </li>
+                {/* <ul>
+                    <li>İsim : {name} <i className="far fa-trash-alt"></i></li>
                     <li>Departman : {department}</li>
                     <li>Maaş : {salary}</li>
                    
-                </ul>
+                </ul> */}
+                <div className="card">
+                    <div className="card-header d-flex justify-content-between">
+            <h4 className="d-inline">{name}</h4>
+            <i className="far fa-trash-alt" style={{cursor:"pointer"}}></i>
+                    </div>
+                    <div className="card-body">
+            <p className="card-text">Maaş : {salary}</p>
+            <p className="card-text">Departman : {department}</p>
+            </div>
+                    </div> 
+
+
                 
             </div>
         )
