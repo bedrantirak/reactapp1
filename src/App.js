ef5440431e1171d6cb51651c361e3abfa20e1665
bedrantirak <bdrntrk@gmail.com>
2020-10-10 11:58:14 +0300

    Create Navbar w Funct.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 
 export default class App extends Component {
   render(){
@@ -8,6 +9,7 @@ export default class App extends Component {
       <h4>App Component</h4>
     {/* <h4 style={{color:"blue",fontSize:"30px"}}>App Component</h4> */}
      {/* <h4 className="header">App Component</h4> */}
+     <Navbar/>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+// import React from "react";
+
+// function Navbar(){
+//     return(
+//         <div>
+//             <h1>User App</h1>
+//         </div>
+//     )
+
+// }
+// export default Navbar;
+
+//------------
+
+// const Navbar = () =>{
+//     return(
+//         <div>
+//             <h1>User App</h1>
+//         </div>
+//     )
+
+// }
+// export default Navbar;
+
+// create with rfc tab =>>
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div>
+            <h3>User App</h3>
+        </div>
+    )
+}
+
+
