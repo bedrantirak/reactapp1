e51309988768587f6c3d6d352a3fd1a5ccba0240
bedrantirak <bdrntrk@gmail.com>
2020-10-12 11:13:39 +0300

    Mounting

---
+++ src/components/Test.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+ class Test extends Component {
+     constructor(props){
+         super(props);
+         this.state = {
+             a: 10
+         }
+         console.log("Constructor");
+     }
+     componentDidMount() {
+         console.log("componentDidMount");
+         //Api istekleri burda yapılır
+         this.setState({
+             a: 20
+         })
+     }
+     
+    render() {
+        console.log("Render");
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+} export default Test;
