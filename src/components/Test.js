223081990b5ff4c8c5f7c29466d61c9d4074a996
bedrantirak <bdrntrk@gmail.com>
2020-10-12 11:33:41 +0300

    Updating

--- src/components/Test.js
+++ src/components/Test.js
@@ -15,6 +15,15 @@ import React, { Component } from 'react'
              a: 20
          })
      }
+     componentDidUpdate(prevProps, prevState) {
+         console.log("component Did Update");
+     }
+     shouldComponentUpdate(nextProps, nextState) {
+         console.log("should Component Update");
+         return true;
+     }
+     
+     
      
     render() {
         console.log("Render");
