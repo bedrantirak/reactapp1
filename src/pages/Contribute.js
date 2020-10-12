8b98c940ea9db015182ba20a882663e0d5a8a9c9
bedrantirak <bdrntrk@gmail.com>
2020-10-12 15:53:48 +0300

    Add Contribute page

---
+++ src/pages/Contribute.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+ function Contribute() {
+    return (
+        <div>
+            <h3>Project Files and Contribute to Project</h3>
+            <p>You can download full <a href="https://github.com/bedrantirak/reactapp1">this</a> project from github.</p>
+        </div>
+    )
+} export default Contribute;
