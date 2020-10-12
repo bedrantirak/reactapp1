a200f7e5e313961f0ff4d804e09a756780f9aecb
bedrantirak <bdrntrk@gmail.com>
2020-10-12 14:56:04 +0300

    React Router

--- src/App.js
+++ src/App.js
@@ -4,21 +4,28 @@ import Navbar from './components/Navbar';
 import Users from "./components/Users";
 import AddUser from "./components/AddUser";
 import Test from "./components/Test";
+import NotFound from "./components/NotFound";
+import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
 
 export default class App extends Component {
   
 
   render(){
   return (
+    <Router>
     <div className="container" >
-      <Test test="deneme"/>
+    <Test test="deneme"/>
      <Navbar 
      title = "User App"
      />
      <hr/>
-     <AddUser/>
-     <Users />
+     <Switch>
+     <Route exact path ="/" component={Users}/>
+     <Route exact path ="/add" component={AddUser}/>
+     <Route  component={NotFound}/>
+     </Switch>
     </div>
+    </Router>
   );
 }
 }
