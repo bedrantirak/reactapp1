721be027945b8b7ae561dd02887b03c83d29f3e3
bedrantirak <bdrntrk@gmail.com>
2020-10-12 14:56:48 +0300

    Add Links and NotFound Component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,3 @@
-// import React from "react";
-
-// function Navbar(){
-//     return(
-//         <div>
-//             <h1>User App</h1>
-//         </div>
-//     )
-
-// }
-// export default Navbar;
-
-//------------
-
-// const Navbar = () =>{
-//     return(
-//         <div>
-//             <h1>User App</h1>
-//         </div>
-//     )
-
-// }
-// export default Navbar;
 
 // create with rfc tab =>>
 import React from 'react'
@@ -30,6 +7,14 @@ export default function Navbar(props) {
     return (
         <div>
             <h3>{props.title}</h3>
+            <ul>
+                <li>
+                    <a  href ="/">Users</a>
+                    </li>
+                <li>
+                    <a href ="/add">Add Users</a>
+                    </li>
+            </ul>
         </div>
     )
 }
