b01af0d226e45dc7f3509c94b84fa5ae26c3542f
bedrantirak <bdrntrk@gmail.com>
2020-10-12 11:13:52 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Users from "./components/Users";
 import AddUser from "./components/AddUser";
+import Test from "./components/Test";
 
 export default class App extends Component {
   
@@ -10,6 +11,7 @@ export default class App extends Component {
   render(){
   return (
     <div className="container" >
+      <Test test="deneme"/>
      <Navbar 
      title = "User App"
      />
